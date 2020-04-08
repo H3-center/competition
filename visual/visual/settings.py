450a7403070b971de1ef23ed61d2c3f94e75feec
owner0220 <owner0220@gmail.com>
2020-04-08 19:32:59 +0900

    20200408 데이터 업로드

--- visual/visual/settings.py
+++ visual/visual/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'Logs',
     'Jobs',
     'accounts',
+    'module',
 ]
 
 MIDDLEWARE = [
--- visual/visual/views.py
+++ visual/visual/views.py
@@ -2,6 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from Jobs.models import Job
 
+
+from module.monitor import get_statistics
 # Create your views here.
 def main(request):
+    statistics=get_statistics()
+    print(statistics)
+    return render(request, 'base.html')
+
+
+def monitorring(request):
+    statistics=get_statistics()
+    print(statistics)
     return render(request, 'base.html')
